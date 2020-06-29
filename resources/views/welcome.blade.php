f167e41efb8c3d1fecbca5d9854175f151f5a45a
Abdoul Hamid COULIBALY <bamanan@users.noreply.github.com>
2020-06-29 20:26:08 +0000

    first commit
    Suppression de fichier

--- resources/views/welcome.blade.php
+++
@@ -1,7 +0,0 @@
-
-<div class="container-fluid"><br>
-    <p>Some example text. Some example text. Some example text. Some example text. Some example text.</p>
-    <p>Some example text. Some example text. Some example text. Some example text. Some example text.</p>
-    <p>Some example text. Some example text. Some example text. Some example text. Some example text.</p>
-    <p>Some example text. Some example text. Some example text. Some example text. Some example text.</p>
-</div>
